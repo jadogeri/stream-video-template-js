2122c2a4afdbb98a5dc412a2df4dde1224fe0499
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2025-07-20 22:25:48 -0500

    Added strem ui template

--- stream-video-template/src/App.js
+++ stream-video-template/src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  CallControls,
+  CallingState,
+  SpeakerLayout,
+  StreamCall,
+  StreamTheme,
+  StreamVideo,
+  StreamVideoClient,
+  useCallStateHooks,
+} from '@stream-io/video-react-sdk';
 
-function App() {
+import '@stream-io/video-react-sdk/dist/css/styles.css';
+import './index.css';
+
+const apiKey = 'mmhfdzb5evj2';
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL3Byb250by5nZXRzdHJlYW0uaW8iLCJzdWIiOiJ1c2VyL0phY2VuX1NvbG8iLCJ1c2VyX2lkIjoiSmFjZW5fU29sbyIsInZhbGlkaXR5X2luX3NlY29uZHMiOjYwNDgwMCwiaWF0IjoxNzUyOTg0NTA3LCJleHAiOjE3NTM1ODkzMDd9.KJUgmwi9erzwAdQFoqCl_GWZm17f-Yb_L_Y0aaYxt9s';
+const userId = 'Jacen_Solo';
+const callId = 'nHcLdakNd7PD';
+
+const user = {
+  id: userId,
+  name: 'Oliver',
+  image: 'https://getstream.io/random_svg/?id=oliver&name=Oliver',
+};
+
+const client = new StreamVideoClient({ apiKey, user, token });
+const call = client.call('default', callId);
+call.join({ create: true });
+//call.camera.enable();
+//call.microphone.enable();
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StreamVideo client={client} >
+      <StreamCall call={call} >
+        <MyUILayout />
+      </StreamCall>
+    </StreamVideo>
   );
 }
 
-export default App;
+export const MyUILayout = () => {
+  const { useCallCallingState } = useCallStateHooks();
+  const callingState = useCallCallingState();
+
+  if (callingState !== CallingState.JOINED) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <StreamTheme>
+      <SpeakerLayout participantsBarPosition='bottom' />
+      <CallControls />
+    </StreamTheme>
+  );
+};
